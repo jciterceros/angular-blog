b7aa624b4b70622f70b9f578869806bdb5270ac7
jciterceros <jciterceros@yahoo.com>
2024-04-28 13:26:25 -0300

    feat: work title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
+import { BigCardComponent } from "./components/big-card/big-card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, MenuTitleComponent, BigCardComponent]
 })
 export class AppComponent {
   title = 'angular-blog';
