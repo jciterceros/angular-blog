549784f45d4fbe706ea229f1ba2935279bff0692
jciterceros <jciterceros@yahoo.com>
2024-04-28 19:47:41 -0300

    feat: Image title and description using data bind

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -7,6 +7,15 @@ import { Component } from '@angular/core';
   templateUrl: './small-card.component.html',
   styleUrl: './small-card.component.css'
 })
-export class SmallCardComponent {
-
+export class SmallCardComponent implements OnInit {
+  @Input() //'https://via.placeholder.com/150'
+  photoCover: string = '';
+  @Input()
+  cardTitle: string = '';
+  @Input()
+  cardDescription: string = '';
+  constructor() { }
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+  }
 }
